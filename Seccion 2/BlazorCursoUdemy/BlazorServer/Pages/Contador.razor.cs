09b0ae6a89c83565456be47be72411c124c9edab
Santiago López Botero <108085502+slopezb16@users.noreply.github.com>
2025-02-04 17:14:32 -0500

    Agregar archivos de proyecto.

---
+++ Seccion 2/BlazorCursoUdemy/BlazorServer/Pages/Contador.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorServer.Pages
+{
+    public partial class Contador
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
